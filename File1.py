3773aebb49bcacd64d5bb071173abc42030560db
V <varunshankar2k6@gmail.com>
2026-06-09 10:43:26 +0530

    File functions

--- File1.py
+++ File1.py
@@ -11,10 +11,12 @@ word= text.split()
 print("Number of words =", len(word))
 
 #Question 3 to print the text from a random position taken as input from user
+import random
 with open(r"C:\Users\sample.txt", "r") as file:
-    text= file.read()
-position = int(input("Enter position"))
-if position>=0 & position<len(text):
-    print("Data from position ",position," is:       ",text[position:])
-else:
-    print("Wrong position try again")
+    file.seek(0, 2)      
+    size = file.tell()   
+    position = random.randint(0, size - 1)
+    file.seek(position)  
+    print("Random Position is at ", position)
+    print("Text from random position is ")
+    print(file.read())
